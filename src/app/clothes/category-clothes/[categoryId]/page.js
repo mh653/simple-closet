d5396e6a1134eb17f919d50b237eae626381acf4
sprout-hatanaka <sprout.hatanaka@gmail.com>
2026-02-15 20:55:14 +0900

    カテゴリ別の服ページを作成

---
+++ src/app/clothes/category-clothes/[categoryId]/page.js
@@ -0,0 +1,68 @@
+import { supabase } from "@/lib/supabaseClient"
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function tagCoode(props) {
+  const params = await props.params
+  const categoryId = params.categoryId
+
+  // カテゴリIDに紐づくカテゴリ名と服データを取得
+  const { data:categoryData } = await supabase
+    .from('t_categories')
+    .select(`
+      name,
+      t_clothes (
+        id,
+        img_path
+      )
+    `)
+    .eq('id', categoryId)
+    .single()
+
+  // 返り値
+  // {
+  //   "name": "ボトムス",
+  //   "t_clothes": [
+  //     {
+  //       "id": 2,
+  //       "img_path": "2.jpg"
+  //     },
+  //     {
+  //       "id": 4,
+  //       "img_path": "4.jpg"
+  //     }
+  //   ]
+  // }
+
+  // ストレージの画像URLを取得する関数
+  const getImageUrl = (imgPath) => {
+    if(!imgPath) return null;
+
+    const { data } = supabase.storage
+      .from('clothes_image')
+      .getPublicUrl(imgPath)
+
+    return data.publicUrl
+  }
+
+  return (
+    <>
+      <h2>カテゴリ別</h2>
+      <p>{categoryData.name}</p>
+      {
+      categoryData.t_clothes.length > 0 ? (
+        categoryData.t_clothes.map((c) => (
+          <Link key={c.id} href={`/clothes-details/${c.id}`}>
+            <div>
+              <p>ID:{c.id}</p>
+              <Image src={getImageUrl(c.img_path)} alt='' width={100} height={100} />
+            </div>
+          </Link>
+        ))
+      ):(
+        <p>このカテゴリに登録されている服がありません。</p>
+      )
+      }
+    </>
+  )
+}
--- src/app/clothes/page.js
+++ src/app/clothes/page.js
@@ -1,4 +1,4 @@
-// クライアントコンポーネントにはasync使えないので、ステートを使っている？
+// クライアントコンポーネントでは async fetch が直接できないので useState でデータを保持？
 
 'use client'
 
@@ -24,9 +24,12 @@ export default function Clothes() {
   const showAll = view === "all";
 
   useEffect(() => {
-    fetchClothes()
-    fetchCategories()
-  }, [])
+    if (showAll) {
+      fetchClothes();
+    } else {
+      fetchCategories();
+    }
+  }, [view])
 
   // t_clothesのデータを取得する関数
   const fetchClothes = async () => {
@@ -60,7 +63,7 @@ export default function Clothes() {
       .order('created_at', {ascending: false})
     setCategories(data || [])
   }
-
+  
   return (
     <>
       {/* 一覧表示を押すと、クエリパラメータが付く */}
@@ -85,7 +88,7 @@ export default function Clothes() {
           {categories.map((category) => (
             <div key={category.id}>
               <p>ID:{category.id}</p>
-              <Link href={`/category-clothes/${category.id}`}>{category.name}</Link>
+              <Link href={`clothes/category-clothes/${category.id}`}>{category.name}</Link>
             </div>
           ))}
         </div>
@@ -93,99 +96,3 @@ export default function Clothes() {
     </>
   );
 }
-
-
-
-// 'use client'
-
-// import { supabase } from "@/lib/supabaseClient";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { useState, useEffect } from "react";
-// import { useSearchParams, useRouter } from "next/navigation";
-
-// export default function Clothes() {
-//   const [clothes, setClothes] = useState([]);
-//   const [categories, setCategories] = useState([]);
-
-//   const router = useRouter();
-
-//   // 今のURLのクエリパラメータを取得するフック（URLが変わると値を更新する）
-//   // /clothes?view=all だと、?view=allの部分がクエリパラメータ
-//   // Reactはフックが内部的に値を更新した時、再レンダリングする
-//   const searchParams = useSearchParams();  
-//   // viewの先が何かを取得する
-//   const view = searchParams.get("view");
-//   // allならtrueが入る
-//   const showAll = view === "all";
-
-//   useEffect(() => {
-//     fetchClothes()
-//     fetchCategories()
-//   }, [])
-
-//   // t_clothesのデータを取得する関数
-//   const fetchClothes = async () => {
-//     const { data } = await supabase
-//       .from('t_clothes')
-//       .select(`
-//         *
-//       `)
-//       .order('created_at', {ascending: false})
-//     setClothes(data || [])
-//   }
-
-//   // ストレージの画像URLを取得する関数
-//   const getImageUrl = (imgPath) => {
-//     if(!imgPath) return null;
-
-//     const { data } = supabase.storage
-//       .from('clothes_image')
-//       .getPublicUrl(imgPath)
-
-//     return data.publicUrl
-//   }
-
-//   // t_categoriesのデータを取得する関数
-//   const fetchCategories = async () => {
-//     const { data } = await supabase
-//       .from('t_categories')
-//       .select(`
-//         *
-//       `)
-//       .order('created_at', {ascending: false})
-//     setCategories(data || [])
-//   }
-
-//   return (
-//     <>
-//       {/* 一覧表示を押すと、クエリパラメータが付く */}
-//       <button onClick={() => router.push("/clothes")}>カテゴリ別</button>
-//       <button onClick={() => router.push("/clothes?view=all")}>一覧表示</button>
-      
-//       {showAll ? (
-//         <div>
-//           <h2>服の一覧</h2>
-//           {clothes.map((c) => (
-//             <Link key={c.id} href={`/clothes-details/${c.id}`}>
-//               <div>
-//                 <p>服ID:{c.id}</p>
-//                 <Image src={getImageUrl(c.img_path)} alt='' width={100} height={100} />
-//               </div>
-//             </Link>
-//           ))}          
-//         </div>
-//       ) : (
-//         <div>
-//           <h2>カテゴリ別</h2>
-//           {categories.map((category) => (
-//             <div key={category.id}>
-//               <p>ID:{category.id}</p>
-//               <Link href={`/category-clothes/${category.id}`}>{category.name}</Link>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </>
-//   );
-// }
